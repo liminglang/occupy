79ca63a5567272a0a996b477e5d6c35219120e5d
liminglang <1432915943@qq.com>
2018-11-19 14:53:46 +0800

    第一版

--- src/config/index.js
+++ src/config/index.js
@@ -1,3 +1,4 @@
-export const baseUrl = 'https://wxapp.ztsafe.com/occupy/'
+export const baseUrl = 'https://wxapp.ztsafe.com/occupy'
+//export const baseUrl = 'http://192.168.115.160:5000'
 export const appId = 'wxe38d185feee3f4bd';
 export const txMapsKey = 'GJOBZ-YSX66-K3SSM-E3MJM-IWPRV-BFFIO';
